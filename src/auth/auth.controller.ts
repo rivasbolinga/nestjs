5e7e136a0c12de5a8ad29e53df01baf0f9f1088b
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-19 10:29:21 +0200

    Modify functions so service is busy with business logic and the controller wit the logic of the request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,11 +6,11 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
   signup() {
-    return 'I am signed up';
+    return this.authService.signup();
   }
 
   @Post('login')
   login() {
-    return 'I am signed in';
+    return this.authService.login();
   }
 }
