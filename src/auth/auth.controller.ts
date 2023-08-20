d8a3e4a2ab5f0e82d0b13f9fbd2b3e181df38070
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-20 10:03:27 +0200

    Use @Body decorator to get the body of the request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup() {
+  signup(@Body() dto: any) {
+    console.log({
+      dto,
+    });
     return this.authService.signup();
   }
 
