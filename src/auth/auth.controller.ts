e89ab782ef149a53a3cca7dc48c9879404714bf3
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-20 11:40:05 +0200

    Add class validators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,13 @@
-import { Controller, Post, Req, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
-
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup(
-    @Body('email') email: string,
-    @Body('password', Parse) password: string,
-  ) {
+  signup(@Body() dto: AuthDto) {
     console.log({
-      email, 
-      typeof
+      dto,
     });
     return this.authService.signup();
   }
