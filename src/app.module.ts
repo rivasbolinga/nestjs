986ddfc6c5189fc01614ed1fffe7596ccecf95fe
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-20 08:49:31 +0200

    Connect Auth module with prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 // Main module would import other modules.
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
 export class AppModule {}
