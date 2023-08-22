ed2b3c5a9931746e46a6779ac3b897a3fe4f1a6f
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-22 08:29:12 +0200

    Configurate nests.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 // Main module would import other modules.
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule, ConfigModule],
 })
 export class AppModule {}
