80c8f1889fb197fa4fcc3ce02437f4bc244e040b
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-22 12:40:55 +0200

    Create contructor

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,5 +1,16 @@
 import { PassportStrategy } from '@nestjs/passport';
 
-export class JwtStrategy extends PassportStrategy(Strategy) {
-
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
 }
